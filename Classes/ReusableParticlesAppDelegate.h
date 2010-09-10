e78fbcae27249f6ef2d8c80a4df5d0f6493ba2a9
Brandon Williams <brandon@opetopic.com>
2010-09-10 16:31:31 -0400

    Initial commit.

---
+++ Classes/ReusableParticlesAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  ReusableParticlesAppDelegate.h
+//  ReusableParticles
+//
+//  Created by Brandon Williams on 9/10/10.
+//  Copyright 2010 Opetopic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ReusableParticlesAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ReusableParticles
+//
+//  Created by Brandon Williams on 9/10/10.
+//  Copyright 2010 Opetopic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
