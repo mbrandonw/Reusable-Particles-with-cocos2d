e126097dc0cdc2ae3b368654235c1f1a12b0c046
Brandon Williams <brandon@opetopic.com>
2010-12-07 22:05:28 -0500

    * Cleaned up some code. * Changed SDK to "newest"

--- Classes/ParticlesScene.m
+++ Classes/ParticlesScene.m
@@ -23,6 +23,7 @@
 	if (! (self = [super init]))
 		return nil;
 	
+	// a couple of preloaded particle system parameter dictionaries (these were made with Particle Designer)
 	explosionDictionaries = [[NSArray alloc] initWithObjects:
 							 [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"ExplosionParticles1" ofType:@"plist"]],
 							 [NSDictionary dictionaryWithContentsOfFile:[[NSBundle mainBundle] pathForResource:@"ExplosionParticles2" ofType:@"plist"]],
@@ -31,11 +32,11 @@
 	
 	self.isTouchEnabled = YES;
 	
+	// create a reusable particle system
 	explosion = [[ARCH_OPTIMAL_PARTICLE_SYSTEM alloc] initWithTotalParticles:300];
 	explosion.reuseParticles = YES;
 	explosion.position = ccp(0.0f,0.0f);
 	[self addChild:explosion];
-	[explosion release];
 	
 	return self;
 }
@@ -44,13 +45,23 @@
 	
 	static int i = 0;
 	
+	// change the parameters of the particle system using one of the preloaded dictionaries
 	[explosion setAttributesWithDictionary:[explosionDictionaries objectAtIndex:(i++ % [explosionDictionaries count])] loadTexture:NO];
 	
-	for (UITouch *touch in touches) {
+	// start an explosion for each touch
+	for (UITouch *touch in touches)
+	{
 		CGPoint p = [[CCDirector sharedDirector] convertToGL:[touch locationInView:[touch view]]];
 		explosion.centerOfGravity = p;
 		[explosion spawnParticles:50];
 	}
+}
+
+-(void) dealloc {
+	
+	[explosion release];
+	
+	[super dealloc];
 }
 
 @end
